0944e23729bf5edd592bb508b3eb6de66d9ac998
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-08-28 21:10:15 +0200

    feat: add find product for delete form

---
+++ SIMS.WinForms/Inventory/frmFindProductForDelete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIMS.WinForms.Inventory
+{
+    public partial class frmFindProductForDelete : Form
+    {
+        public frmFindProductForDelete()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
