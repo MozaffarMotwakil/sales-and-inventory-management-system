7872599d1232e884529afed45584ba4cd07a901a
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-09-22 07:51:26 +0200

    feat: complete add edit organization control

---
+++ SIMS.WinForms/Organizations/ctrAddEditOrganization.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Forms;
+using BusinessLogic;
+using DVLD.WinForms.Utils;
+using SIMS.WinForms.Parties;
+using SIMS.WinForms.People;
+
+namespace SIMS.WinForms.Organizations
+{
+    public partial class ctrAddEditOrganization : UserControl
+    {
+        private clsPerson _ContactPerson;
+        public clsPerson ContactPerson 
+        {
+            get
+            {
+                return _ContactPerson;
+            }
+            set
+            {
+                if (value is null)
+                {
+                    return;
+                }
+
+                _ContactPerson = value;
+                txtContactPerson.Text = value.PartyName;
+                llAddContactPerson.Text = "تعديل بيانات جهة التواصل داخل المنظمة";
+            }
+        }
+
+        public clsOrganization Organization
+        {
+            get
+            {
+                return new clsOrganization(
+                    ctrAddEditParty.PartyName,
+                    ctrAddEditParty.CountryID,
+                    ctrAddEditParty.Phone,
+                    ctrAddEditParty.Email,
+                    ctrAddEditParty.Address,
+                    ContactPerson
+                    );
+            }
+            set
+            {
+                if (value is null)
+                {
+                    return;
+                }
+
+                ctrAddEditParty.PartyName = value.PartyName;
+                ctrAddEditParty.CountryID = value.CountryID;
+                ctrAddEditParty.Phone = value.Phone;
+                ctrAddEditParty.Email = value.Email;
+                ctrAddEditParty.Address = value.Address;
+                ContactPerson = value.ContactPerson;
+            }
+        }
+
+        public ctrAddEditOrganization()
+        {
+            InitializeComponent();
+        }
+
+        private void llAddContactPerson_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmAddEditPerson addEditContactPerson;
+
+            if (ContactPerson is null)
+            {
+                addEditContactPerson = new frmAddEditPerson();
+                addEditContactPerson.FormMode = enMode.Add;
+            }
+            else
+            {
+                addEditContactPerson = new frmAddEditPerson(ContactPerson);
+                addEditContactPerson.FormMode = enMode.Edit;
+            }
+
+            addEditContactPerson.SaveSuccess += AddEditContactPerson_SaveSuccess;
+            addEditContactPerson.PersonType = clsParty.enPartyType.ContactPerson;
+            addEditContactPerson.ShowDialog();
+        }
+
+        private void AddEditContactPerson_SaveSuccess(clsPerson contactPerson)
+        {
+            ContactPerson = contactPerson;
+        }
+
+        private void btnDeleteContactPerson_Click(object sender, EventArgs e)
+        {
+            if (ContactPerson != null)
+            {
+                if (clsFormMessages.Confirm("هل أنت متأكد من أنك تريد حذف جهة التواصل داخل هذه المنظمة ؟", messageBoxIcon: MessageBoxIcon.Warning))
+                {
+                    ContactPerson = null;
+                    txtContactPerson.Text = string.Empty;
+                    llAddContactPerson.Text = "إضافة جهة تواصل داخل المنظمة";
+                }
+            }
+            else
+            {
+                clsFormMessages.ShowError("ليس هناك جهة تواصل داخل هذه المنظمة لحذفها");
+            }
+        }
+
+    }
+}
