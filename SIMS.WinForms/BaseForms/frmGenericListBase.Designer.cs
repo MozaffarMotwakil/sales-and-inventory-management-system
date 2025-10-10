4c96d3fd6b43693d7d7f7db7965be1de426b7ac8
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-10-10 12:01:47 +0200

    feat: add GenericListBase form

---
+++ SIMS.WinForms/BaseForms/frmGenericListBase.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace SIMS.WinForms.BaseForms
+{
+    partial class frmGenericListBase<TManager, TEntity>
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblSearchHintText = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblTotalRecords = new System.Windows.Forms.Label();
+            this.lblTotalRecordsText = new System.Windows.Forms.Label();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.dgvEntitiesList = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEntitiesList)).BeginInit();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSearchHintText
+            // 
+            this.lblSearchHintText.AutoSize = true;
+            this.lblSearchHintText.BackColor = System.Drawing.Color.White;
+            this.lblSearchHintText.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.lblSearchHintText.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearchHintText.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblSearchHintText.Location = new System.Drawing.Point(798, 11);
+            this.lblSearchHintText.Name = "lblSearchHintText";
+            this.lblSearchHintText.Size = new System.Drawing.Size(47, 16);
+            this.lblSearchHintText.TabIndex = 36;
+            this.lblSearchHintText.Text = "الرسالة";
+            this.lblSearchHintText.Click += new System.EventHandler(this.pictureBoxAndSearchHintText_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.pictureBox.Image = global::SIMS.WinForms.Properties.Resources.search_icon;
+            this.pictureBox.Location = new System.Drawing.Point(766, 6);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(29, 26);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 37;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBoxAndSearchHintText_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(794, 6);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(94, 26);
+            this.txtSearch.TabIndex = 34;
+            this.txtSearch.TextChanged += new System.EventHandler(this.SearchTextChanged);
+            // 
+            // lblTotalRecords
+            // 
+            this.lblTotalRecords.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTotalRecords.AutoSize = true;
+            this.lblTotalRecords.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalRecords.Location = new System.Drawing.Point(160, 435);
+            this.lblTotalRecords.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalRecords.Name = "lblTotalRecords";
+            this.lblTotalRecords.Size = new System.Drawing.Size(28, 16);
+            this.lblTotalRecords.TabIndex = 43;
+            this.lblTotalRecords.Text = "N/A";
+            this.lblTotalRecords.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTotalRecords.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HideEntityInfo);
+            // 
+            // lblTotalRecordsText
+            // 
+            this.lblTotalRecordsText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTotalRecordsText.AutoSize = true;
+            this.lblTotalRecordsText.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalRecordsText.Location = new System.Drawing.Point(9, 435);
+            this.lblTotalRecordsText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTotalRecordsText.Name = "lblTotalRecordsText";
+            this.lblTotalRecordsText.Size = new System.Drawing.Size(143, 16);
+            this.lblTotalRecordsText.TabIndex = 42;
+            this.lblTotalRecordsText.Text = "إجمالي عدد السجلات:";
+            this.lblTotalRecordsText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTotalRecordsText.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HideEntityInfo);
+            // 
+            // dgvEntitiesList
+            // 
+            this.dgvEntitiesList.AllowUserToAddRows = false;
+            this.dgvEntitiesList.AllowUserToDeleteRows = false;
+            this.dgvEntitiesList.AllowUserToOrderColumns = true;
+            this.dgvEntitiesList.AllowUserToResizeRows = false;
+            this.dgvEntitiesList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvEntitiesList.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvEntitiesList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvEntitiesList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEntitiesList.ContextMenuStrip = this.contextMenuStrip;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvEntitiesList.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvEntitiesList.Location = new System.Drawing.Point(12, 318);
+            this.dgvEntitiesList.MultiSelect = false;
+            this.dgvEntitiesList.Name = "dgvEntitiesList";
+            this.dgvEntitiesList.ReadOnly = true;
+            this.dgvEntitiesList.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvEntitiesList.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvEntitiesList.RowHeadersVisible = false;
+            this.dgvEntitiesList.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvEntitiesList.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvEntitiesList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvEntitiesList.Size = new System.Drawing.Size(876, 114);
+            this.dgvEntitiesList.TabIndex = 40;
+            this.dgvEntitiesList.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvEntitiesList_CellMouseClick);
+            this.dgvEntitiesList.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvEntitiesList_CellMouseDoubleClick);
+            this.dgvEntitiesList.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dgvEntitiesList_DataBindingComplete);
+            this.dgvEntitiesList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dgvEntitiesList_KeyDown);
+            this.dgvEntitiesList.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HideEntityInfo);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.contextMenuStrip.Size = new System.Drawing.Size(197, 102);
+            this.contextMenuStrip.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip_Opening);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editToolStripMenuItem.Image = global::SIMS.WinForms.Properties.Resources.edit;
+            this.editToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(196, 38);
+            this.editToolStripMenuItem.Text = "تعديل";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteToolStripMenuItem.Image = global::SIMS.WinForms.Properties.Resources.delete;
+            this.deleteToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(196, 38);
+            this.deleteToolStripMenuItem.Text = "حذف";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // frmGenericListBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 460);
+            this.Controls.Add(this.lblTotalRecords);
+            this.Controls.Add(this.lblTotalRecordsText);
+            this.Controls.Add(this.dgvEntitiesList);
+            this.Controls.Add(this.lblSearchHintText);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.txtSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmGenericListBase";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmGenericListBase_FormClosed);
+            this.Load += new System.EventHandler(this.frmGenericListBase_Load);
+            this.Shown += new System.EventHandler(this.frmGenericListBase_Shown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HideEntityInfo);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEntitiesList)).EndInit();
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        protected System.Windows.Forms.Label lblTotalRecordsText;
+        protected System.Windows.Forms.ToolTip toolTip;
+        protected System.Windows.Forms.DataGridView dgvEntitiesList;
+        protected System.Windows.Forms.TextBox txtSearch;
+        protected System.Windows.Forms.Label lblSearchHintText;
+        protected System.Windows.Forms.PictureBox pictureBox;
+        protected System.Windows.Forms.Label lblTotalRecords;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
---
+++ SIMS.WinForms/BaseForms/frmGenericListBase.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using BusinessLogic.Interfaces;
+using DVLD.WinForms.Utils;
+
+namespace SIMS.WinForms.BaseForms
+{
+    public partial class frmGenericListBase<TManager, TEntity> : Form
+        where TManager : IEntityListManager<TEntity>, new()
+    {
+        protected TManager Manager { get; private set; }
+        protected Control EntityInfoControl { get; set; }
+        protected string EntityName { get; set; }
+        protected string Filter { get; set; }
+        protected string SearchHintMessage
+        {
+            get => lblSearchHintText.Text;
+            set => lblSearchHintText.Text = value;
+        }
+
+        public frmGenericListBase()
+        {
+            InitializeComponent();
+            Manager = new TManager();
+            Manager.EntitySaved += EntitySavedEvent; ;
+            Manager.EntityDeleted += EntityDeletedEvent; ;
+        }
+
+        protected virtual void EntitySavedEvent(object sender, EntitySavedEventArgs e)
+        {
+            txtSearch.Text = string.Empty;
+            dgvEntitiesList.DataSource = Manager.GetAll();
+            lblTotalRecords.Text = dgvEntitiesList.Rows.Count.ToString();
+
+            if (e.OperationMode == BusinessLogic.enMode.Update && EntityInfoControl.Visible)
+            {
+                HandleEntityInfoDisplay(e.EntityID);
+            }
+            else
+            {
+                EntityInfoControl.Visible = false;
+            }
+        }
+
+        protected virtual void EntityDeletedEvent(object sender, EntityDeletedEventArgs e)
+        {
+            txtSearch.Text = string.Empty;
+            EntityInfoControl.Visible = false;
+            dgvEntitiesList.DataSource = Manager.GetAll();
+            lblTotalRecords.Text = dgvEntitiesList.Rows.Count.ToString();
+        }
+
+        private void frmGenericListBase_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            ResetColumnsOfDGV();
+        }
+
+        private void frmGenericListBase_Shown(object sender, EventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        protected virtual void LoadData()
+        {
+            dgvEntitiesList.DataSource = Manager.GetAll();
+            lblTotalRecords.Text = dgvEntitiesList.Rows.Count.ToString();
+            dgvEntitiesList.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 8, FontStyle.Bold);
+        }
+
+        protected virtual void ResetColumnsOfDGV() { }
+
+        protected virtual void SearchTextChanged(object sender, EventArgs e)
+        {
+            lblSearchHintText.Visible = string.IsNullOrEmpty(txtSearch.Text);
+
+            DataView entitiesList = (dgvEntitiesList.DataSource as DataTable).DefaultView;
+
+            try
+            {
+                entitiesList.RowFilter = Filter;
+            }
+            catch (Exception) 
+            {
+                // في حال رمي إستثناء بسبب إدخال رموز غير صالحة فلا حاجة لعرض رسالة خطأ أو إيقاف تجربة المستخدم
+            } 
+                
+            lblTotalRecords.Text = entitiesList.Count.ToString();
+        }
+
+        private void pictureBoxAndSearchHintText_Click(object sender, EventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TEntity product = Manager.Find(clsFormHelper.GetSelectedRowID(dgvEntitiesList));
+
+            if (product == null)
+            {
+                clsFormMessages.ShowError($"لم يتم العثور على {EntityName}");
+                return;
+            }
+
+            Form editEntity = CreateEditForm(product);
+
+           editEntity.ShowDialog();
+        }
+
+        protected virtual Form CreateEditForm(TEntity entity) 
+        {
+            throw new NotImplementedException();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dgvEntitiesList.SelectedRows.Count == 0)
+            {
+                clsFormMessages.ShowError($"لم يتم العثور على {EntityName}");
+                return;
+            }
+
+            DeleteSelectedEntity();
+        }
+
+        private void dgvEntitiesList_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex == -1)
+            {
+                return;
+            }
+
+            if (dgvEntitiesList.SelectedRows.Count == 0)
+            {
+                clsFormMessages.ShowError($"لم يتم العثور على {EntityName}");
+                return;
+            }
+
+            if (e.Button is MouseButtons.Left)
+            {
+                ShowSelectedEntityInfo();
+            }
+        }
+
+        private void dgvEntitiesList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (dgvEntitiesList.SelectedRows.Count == 1)
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    ShowSelectedEntityInfo();
+                }
+                else if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back)
+                {
+                    DeleteSelectedEntity();
+                }
+            }
+        }
+
+        private void dgvEntitiesList_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dgvEntitiesList.ClearSelection();
+        }
+
+        private void dgvEntitiesList_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button is MouseButtons.Right && e.RowIndex >= 0)
+            {
+                dgvEntitiesList.Rows[e.RowIndex].Selected = true;
+                dgvEntitiesList.Columns[e.ColumnIndex].Selected = true;
+            }
+        }
+
+        protected void HideEntityInfo(object sender, MouseEventArgs e)
+        {
+            dgvEntitiesList.ClearSelection();
+
+            if (sender != dgvEntitiesList && EntityInfoControl != null)
+            {
+                EntityInfoControl.Visible = false;
+            }
+        }
+
+        private void contextMenuStrip_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            clsFormHelper.PreventContextMenuOnEmptyClick(dgvEntitiesList, e);
+        }
+
+        private void ShowSelectedEntityInfo()
+        {
+            HandleEntityInfoDisplay(clsFormHelper.GetSelectedRowID(dgvEntitiesList));
+        }
+
+        protected virtual void HandleEntityInfoDisplay(int entityID) { }
+
+        protected virtual void DeleteSelectedEntity()
+        {
+            if (clsFormMessages.Confirm($"هل أنت متأكد من أنك تريد حذف هذا {EntityName} ؟", messageBoxIcon: MessageBoxIcon.Warning, messageBoxDefaultButton: MessageBoxDefaultButton.Button2))
+            {
+                if (Manager.Delete(clsFormHelper.GetSelectedRowID(dgvEntitiesList)))
+                {
+                    clsFormMessages.ShowSuccess($"تم حذف {EntityName} بنجاح");
+                }
+                else
+                {
+                    clsFormMessages.ShowError($"فشلت عملية حذف {EntityName}");
+                }
+            }
+        }
+
+        private void frmGenericListBase_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Manager.EntitySaved -= EntitySavedEvent;
+            Manager.EntityDeleted -= EntityDeletedEvent;
+        }
+
+    }
+}
